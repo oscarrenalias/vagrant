3f4e4e672c33711f9594183a9fc23ebae94fdf4b
Oscar Renalias <oscar@renalias.net>
2013-02-12 00:16:18 +0200

    Added the needed rules for iptables so that we can connect to Redis from the outside of the VM

--- redis/manifests/init.pp
+++ redis/manifests/init.pp
@@ -1,3 +1,9 @@
 class { 'base': } ->
 class { 'gcc': } ->
 class { 'redis': version => '2.6.5', }
+
+# allow traffic via the CentOS firewall to Redis
+firewall { "999 Redis traffic":
+  action => "accept",
+  dport => $redis::params::redis_port,
+}
