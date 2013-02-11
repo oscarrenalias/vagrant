1759d44ed67106d12041495d10615b2e9458d76a
Oscar Renalias <oscar@renalias.net>
2013-02-12 00:01:28 +0200

    The vagrant module no longer exists in the puppet repo, so we'll put it back here

--- postgresql/manifests/init.pp
+++ postgresql/manifests/init.pp
@@ -1,2 +1,28 @@
-require base
-require vagrant::postgresql
+#
+# Sets up a postgresql database, for Vagrant VMs
+#
+class postgresql {
+  class { "postgresql::server":
+    config_hash => {
+      "listen_addresses" => "*",
+      "ip_mask_deny_postgres_user" => "1.1.1.1/1",
+      "ip_mask_allow_all_users" => "0.0.0.0/0",
+      "manage_redhat_firewall" => true,
+    }
+  }
+  
+  postgresql::db { "database":
+    user => "db_user",
+    password => "db_password",
+    grant => [ "all" ],
+  }
+  
+  $pg_hba_conf_path = $operatingsystem ? {
+    "CentOS" => "/var/lib/pgsql/data/pg_hba.conf",
+    "Ubuntu" => "/var/lib/postgres/8.4/data/pg_hba.conf",
+    default => fail("Unsupported OS: $operatingsystem"),
+  }
+}
+
+class { 'base': } ->
+class { 'postgresql': }
