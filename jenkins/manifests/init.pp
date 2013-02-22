ff4daf3dccce94b75047201e7dda8c789e63c5bf
Oscar Renalias <oscar@renalias.net>
2013-02-22 23:49:15 +0200

    Added a rule to let traffic to port 8080 flow, and swithced to use the jenkins_node module

--- jenkins/manifests/init.pp
+++ jenkins/manifests/init.pp
@@ -3,4 +3,10 @@ class { "java":
   type => "jdk",
   version => "1.7.0",
 } ->
-class { "jenkins": }
+class { "jenkins_node": }
+
+# allow traffic via the CentOS firewall to Jenkins
+firewall { "999 Jenkins traffic":
+  action => "accept",
+  dport => "8080",
+}
