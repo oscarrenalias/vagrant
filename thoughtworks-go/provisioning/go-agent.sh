6e712fe7e40badf3345f469ef2bb41989dd7176e
Oscar Renalias <oscar@renalias.net>
2014-03-02 21:23:29 +0200

    Added a comment.

--- thoughtworks-go/provisioning/go-agent.sh
+++ thoughtworks-go/provisioning/go-agent.sh
@@ -1,3 +1,7 @@
+#
+# This provioning script can be used to set up additional Go agents, though it is not currently used
+# by the existing Vagrant file
+#
 echo Downloading Go server and agent...
 cd /tmp && curl -O http://download01.thoughtworks.com/go/13.4.1/ga/go-agent-13.4.1-18342.noarch.rpm
 
